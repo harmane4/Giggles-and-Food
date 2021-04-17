f8ca95bbc11a82d409eff7e8756caa7f042ed49f
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-04-17 22:25:26 +0800

    C.10.10 - updated Javascript to localstorage

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,42 @@ burgerIcon.addEventListener('click', () => {
   navbarMenu.classList.toggle('is-active')
 })
 
+const storeName = document.querySelector(".input");
+const storeReview = document.querySelector(".textarea");
+const storedInformation = localStorage.getItem('Name');
+const storedInfoReview = localStorage.getItem('Review')
+const button = document.querySelector(".button");
+
+if (storeName) {
+  storeName.textContent = storedInformation
+
+}
+
+storeName.addEventListener('input', information => {
+  console.log(information.target.value)
+  storeName.textContent = information.target.value
+})
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('Name', storeName.textContent);
+  localStorage.setItem('Review', storeReview.textContent);
+}
+
+button.addEventListener('click', saveToLocalStorage)
+
+
+if (storeReview) {
+  storeReview.textContent = storedInfoReview
+
+}
+
+storeReview.addEventListener('input', information => {
+  console.log(information.target.value)
+  storeReview.textContent = information.target.value
+})
+
+button.addEventListener('click', saveToLocalStorage)
+
 
  
  
