60100354d5b5b70de371ca0322503dda19c8dc84
Elise Harman <elise_harman@hotmail.com>
2021-04-18 12:16:13 +0800

    C.10.10 Changed html and js to try and have resturant name load to local storage

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,26 @@ burgerIcon.addEventListener('click', () => {
   navbarMenu.classList.toggle('is-active')
 })
 
+//local storage
 const storeName = document.querySelector(".input");
 const storeReview = document.querySelector(".textarea");
+// const storeRestaurantName = document.querySelector(".workplease")
 const storedInformation = localStorage.getItem('Name');
 const storedInfoReview = localStorage.getItem('Review')
+const storedInfoRestaurant = localStorage.getItem('Restaurant')
 const button = document.querySelector(".button");
+
 
 if (storeName) {
   storeName.textContent = storedInformation
-
 }
+if (storeReview) {
+  storeReview.textContent = storedInfoReview
+}
+
+// if (storeRestaurantName) {
+//   storeRestaurantName.value = storedInfoRestaurant
+// }
 
 storeName.addEventListener('input', information => {
   console.log(information.target.value)
@@ -25,24 +35,29 @@ storeName.addEventListener('input', information => {
 const saveToLocalStorage = () => {
   localStorage.setItem('Name', storeName.textContent);
   localStorage.setItem('Review', storeReview.textContent);
+  localStorage.setItem('Restaurant',storeRestaurantName.textContent);
 }
 
 button.addEventListener('click', saveToLocalStorage)
-
-
-if (storeReview) {
-  storeReview.textContent = storedInfoReview
-
-}
 
 storeReview.addEventListener('input', information => {
   console.log(information.target.value)
   storeReview.textContent = information.target.value
 })
 
-button.addEventListener('click', saveToLocalStorage)
 
+button.addEventListener('click', renderLocalStorageInfo)
 
  
- 
+//  function renderLocalStorageInfo() {
+//   document.getElementById("nameChange").innerHTML = storedInformation;
+//  }
+
+//  renderLocalStorageInfo() 
+
+
+
+
+
+
  
