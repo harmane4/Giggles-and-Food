80f40782862eba86605b15ccf982375884cbe443
Elise Harman <elise_harman@hotmail.com>
2021-04-19 11:12:57 +0800

    C.10.10 Changed local storage

--- assets/script.js
+++ assets/script.js
@@ -9,105 +9,136 @@ burgerIcon.addEventListener('click', () => {
 //local storage
 const storeName = document.querySelector(".input");
 const storeReview = document.querySelector(".textarea");
+console.log(storeReview.textContent)
 const storeRestaurantName = document.querySelector(".restaurantChoices")
-const storedInformation = localStorage.getItem('Name');
-const storedInfoReview = localStorage.getItem('Review')
-const storedInfoRestaurant = localStorage.getItem('Restaurant')
+// const storedInformation = localStorage.getItem('Name');
+// const storedInfoReview = localStorage.getItem('Review')
+// const storedInfoRestaurant = localStorage.getItem('Restaurant')
 const button = document.querySelector(".button");
 const reviews = document.querySelector(".reviews")
 const reviewList = document.querySelector(".reviewList")
 var listElement = document.createElement("li");
 var listOfReviews;
 
+getSearchHistoryFromLocalStorage();
+renderSearchHistoryResults();
+button.addEventListener("click", saveSearchHistoryToLocalStorage(storeReview.textContent));
 
+//LOCAL STORAGE FUNCTIONS
 
-document.getElementById("nameChange").innerHTML = storedInformation 
-document.getElementById("reviewContent").innerHTML = storedInfoReview
-listElement.textContent = localStorage.getItem('Review');
-
-
-
-if (storeName) {
-  storeName.textContent = storedInformation
-}
-if (storeReview) {
-  storeReview.textContent = storedInfoReview
+function saveSearchHistoryToLocalStorage(review) {
+  //review is the parameter that represents the argument
+  listOfReviews.push(review);
+  localStorage.setItem("review", JSON.stringify(listOfReviews));
 }
 
-storeName.addEventListener('input', information => {
-  console.log(information.target.value)
-  storeName.textContent = information.target.value
-})
-
-//suggestion
-const saveToLocalStorage = () => {
-  localStorage.setItem('Name', storeName.textContent);
-  // localStorage.setItem('Review', storeReview.textContent);
-  var savedReviews = JSON.parse( localStorage.getItem("Review") )
-  //for non-string data, we must parse data back into arrays when getItem.
-  //we'll assume that savedReviews is then an array of reviews.
-  if( savedReviews ){
-    //if something's already in localStorage.Review...
-    //1. push new review into savedReviews
-    //2. setItem into localStorage to replace the old data.
+function getSearchHistoryFromLocalStorage() {
+  var reviewSearchHistory = JSON.parse(localStorage.getItem("review"));
+  if (!reviewSearchHistory) {
+    // checking to see if anything exsists in local storage
+    listOfReviews = [];
   } else {
-    //nothing is saved, so we put the one review into an array and store THAT.
-    localStorage.setItem("Review", [storeReview.textContent])
+    listOfReviews = reviewSearchHistory;
   }
-
-
-//end
-
-
-
-
-const saveToLocalStorage = () => {
-  localStorage.setItem('Name', storeName.textContent);
-  localStorage.setItem('Review', storeReview.textContent);
-  localStorage.setItem('Restaurant', storeRestaurantName.value);
-
-  thankYouMessage()
-  renderReviews()
-  clearFields () 
 }
 
-button.addEventListener('click', saveToLocalStorage)
-
-
-storeReview.addEventListener('input', information => {
-  console.log(information.target.value)
-  storeReview.textContent = information.target.value
-})
-
-function clearFields () {
-  storeName.value = "";
-  storeReview.value = "";
+function renderSearchHistoryResults() {
+  for (let index = 0; index < listOfReviews.length; index++) {
+    const review = listOfReviews[index];
+    var listElement = document.createElement("li");
+    listElement.textContent = review;
+    reviewList.appendChild(listElement);
+  }
 }
 
 
-function thankYouMessage () {
+// document.getElementById("nameChange").innerHTML = storedInformation 
+// document.getElementById("reviewContent").innerHTML = storedInfoReview
+// listElement.textContent = localStorage.getItem('Review');
 
-  //if (storeName.value === "") {
-    //console.log("Please enter name")
-  //} if (storeReview.value === "") {
-    //console.log("Please leave a review")
-  //} else {
-    if (storeReview) {
-    console.log("Thank you for your review")
-    storeReview.placeholder = "Thank you for your review";
-    document.getElementById("nameChange").innerHTML = localStorage.getItem('Name') 
-    document.getElementById("reviewContent").innerHTML = localStorage.getItem('Review')
-    }
+
+
+// if (storeName) {
+//   storeName.textContent = storedInformation
+// }
+// if (storeReview) {
+//   storeReview.textContent = storedInfoReview
+// }
+
+// storeName.addEventListener('input', information => {
+//   console.log(information.target.value)
+//   storeName.textContent = information.target.value
+// })
+
+// //suggestion
+// const saveToLocalStorage = () => {
+//   localStorage.setItem('Name', storeName.textContent);
+//   // localStorage.setItem('Review', storeReview.textContent);
+//   var savedReviews = JSON.parse( localStorage.getItem("Review") )
+//   //for non-string data, we must parse data back into arrays when getItem.
+//   //we'll assume that savedReviews is then an array of reviews.
+//   if( savedReviews ){
+//     //if something's already in localStorage.Review...
+//     //1. push new review into savedReviews
+//     //2. setItem into localStorage to replace the old data.
+//   } else {
+//     //nothing is saved, so we put the one review into an array and store THAT.
+//     localStorage.setItem("Review", [storeReview.textContent])
+//   }
+
+
+// //end
+
+
+
+
+// const saveToLocalStorage = () => {
+//   localStorage.setItem('Name', storeName.textContent);
+//   localStorage.setItem('Review', storeReview.textContent);
+//   localStorage.setItem('Restaurant', storeRestaurantName.value);
+
+//   thankYouMessage()
+//   renderReviews()
+//   clearFields () 
+// }
+
+// button.addEventListener('click', saveToLocalStorage)
+
+
+// storeReview.addEventListener('input', information => {
+//   console.log(information.target.value)
+//   storeReview.textContent = information.target.value
+// })
+
+// function clearFields () {
+//   storeName.value = "";
+//   storeReview.value = "";
+// }
+
+
+// function thankYouMessage () {
+
+//   //if (storeName.value === "") {
+//     //console.log("Please enter name")
+//   //} if (storeReview.value === "") {
+//     //console.log("Please leave a review")
+//   //} else {
+//     if (storeReview) {
+//     console.log("Thank you for your review")
+//     storeReview.placeholder = "Thank you for your review";
+//     document.getElementById("nameChange").innerHTML = localStorage.getItem('Name') 
+//     document.getElementById("reviewContent").innerHTML = localStorage.getItem('Review')
+//     }
    
-  }
+//   }
   
-  function renderReviews() {
-    for (let index = 0; index < 1; index++) {
-      var listElement = document.createElement("li");
-      listElement.textContent = localStorage.getItem('Review');
-      reviewList.appendChild(listElement);
-    }
-  }
+//   function renderReviews() {
+//     for (let index = 0; index < 1; index++) {
+//       var listElement = document.createElement("li");
+//       listElement.textContent = localStorage.getItem('Review');
+//       reviewList.appendChild(listElement);
+//     }
+//   }
 
 
 // Store Information
